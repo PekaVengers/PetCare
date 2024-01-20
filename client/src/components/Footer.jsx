46b5b90db915ff5587ae08be85a0837a9f44d65f
singodiyashubham87 <singodiyashubham87@gmail.com>
2024-01-20 17:58:52 +0530

    style: fix outlet height, style navbar and footer and register page

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,5 +1,11 @@
 export default function Footer() {
   return (
-    <div>Footer</div>
+    <footer className="flex justify-between px-[5rem] bg-gray-700 py-[1rem] text-[1.3rem] font-primary text-white">
+    <span>All Copyright Reserved &copy;</span>
+    <ul className="footer flex gap-[1rem]">
+        <a href="#">Github</a>
+        <a href="#">E-mail</a>
+    </ul>
+  </footer>
   )
 }
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,4 +1,5 @@
-import { Form } from "react-router-dom"
+import { Form } from "react-router-dom";
+import bgImage from "../assets/images/loginBg.png";
 
 export async function action({params, request}) {
   const formData = await request.formData();
@@ -13,8 +14,8 @@ export async function action({params, request}) {
 
 export default function Register() {
   return (
-    <div>
-      <Form method="POST" className="max-w-md mx-auto p-4 bg-white shadow-md rounded-md">
+    <div className="h-screen bg-red-600 bg-no-repeat bg-cover bg-center flex justify-center items-center border-2 border-black" style={{background: `url(${bgImage})`}}>
+      <Form method="POST" className="max-w-md mx-auto p-4 bg-white shadow-md rounded-md bg-gray-700">
         <input required className="mb-4 w-full p-2 border border-gray-300 rounded-md" type="text" name="name" placeholder="Name"/>
         <input required className="mb-4 w-full p-2 border border-gray-300 rounded-md" type="email" name="email" placeholder="Email"/>
         <input required className="mb-4 w-full p-2 border border-gray-300 rounded-md" type="text" name="location" placeholder="Location"/>
@@ -22,7 +23,6 @@ export default function Register() {
         <input required className="mb-4 w-full p-2 border border-gray-300 rounded-md" type="password" name="confirmPassword" placeholder="Confirm Password"/>
         <button type="submit" className="w-full p-2 bg-blue-500 text-white rounded-md hover:bg-blue-700">Register</button>
       </Form>
-
     </div>
   )
 }
