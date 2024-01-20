bc074a4f84cbf59d4eacd7d7c93cc9ebe1b74016
singodiyashubham87 <singodiyashubham87@gmail.com>
2024-01-20 09:59:28 +0530

    feature: route setup

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,34 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Layout from "./components/Layout";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Error from "./components/Error";
+import App from './App';
+
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout/>,
+    errorElement: <Error/>,
+    children: [
+      {
+        path: "/",
+        element: <App/>
+      },
+      {
+        path: "/about",
+        element: <About/>,
+      },
+      {
+        path: "/contact",
+        element: <Contact/>,
+      }
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <RouterProvider router={appRouter} />
 )
