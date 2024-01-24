9c5bf3dfbca44a0b1a4ff0b7e6f9a175a4db1318
singodiyashubham87 <singodiyashubham87@gmail.com>
2024-01-25 01:54:56 +0530

    feat: homepage basic layout and component addition done.

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,7 +1,19 @@
+import Banner from "../assets/images/banner2.png";
+import Hero from "../components/Hero";
+import CTA from "../components/CTA";
+import Testimonial from "../components/Testimonial";
+
 export default function Home() {
   return (
-    <div className="w-full min-h-screen bg-[#FEFFC0]">
-     
+    <div className="w-full min-h-screen bg-[#FEFFC0] relative">
+      <div className="bannerImage w-full">
+     <img src={Banner} alt="banner_image" className="w-full h-full object-cover" />
+      </div>
+      <Hero/>
+      <h1 className="uppercase font-primary mb-[2rem] text-[4rem] text-center font-semibold">Call To Action</h1>
+      <CTA/>
+      <h1 className="uppercase font-primary my-[2rem] text-[4rem] text-center font-semibold">Testimonial</h1>
+      <Testimonial/>
     </div>
   )
 }
