45c8c9a4a83e9797c8a2c46c2af70abf2847cf7d
singodiyashubham87 <singodiyashubham87@gmail.com>
2024-01-25 10:47:31 +0530

    rejuvenate: register page

--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,9 +1,7 @@
 import { Form } from "react-router-dom";
-import bgImage from "../assets/images/loginBg.png";
-import logo from "../assets/images/petcareLogo.png";
 
 // eslint-disable-next-line no-unused-vars
-export async function action({params, request}) {
+export async function action({ params, request }) {
   const formData = await request.formData();
   const name = formData.get("name");
   const location = formData.get("location");
@@ -16,16 +14,73 @@ export async function action({params, request}) {
 
 export default function Register() {
   return (
-    <div className="w-full h-screen bg-no-repeat bg-cover bg-center flex flex-col justify-center items-center gap-[1rem] border-2 border-black" style={{background: `url(${bgImage})`}}>
-      <img src={logo} alt="logo" className="w-[400px] h-auto"/>
-      <Form method="POST" className="max-w-md mx-auto p-4 bg-white shadow-md rounded-md bg-[#080909]">
-        <input required className="mb-4 w-full p-2 border border-gray-300 rounded-md outline-none" type="text" name="name" placeholder="Name"/>
-        <input required className="mb-4 w-full p-2 border border-gray-300 rounded-md outline-none" type="email" name="email" placeholder="Email"/>
-        <input required className="mb-4 w-full p-2 border border-gray-300 rounded-md outline-none" type="text" name="location" placeholder="Location"/>
-        <input required className="mb-4 w-full p-2 border border-gray-300 rounded-md outline-none" type="password" name="password" placeholder="Password"/>
-        <input required className="mb-4 w-full p-2 border border-gray-300 rounded-md outline-none" type="password" name="confirmPassword" placeholder="Confirm Password"/>
-        <button type="submit" className="w-full p-2 bg-[#f8aa26] text-[#080909] rounded-md hover:opacity-[0.8] hover:text-[#080909] uppercase font-semibold">Register</button>
+    <div className="w-full h-screen bg-[#FEFFC0] flex flex-col justify-center items-center gap-[1rem]">
+      <Form
+        method="POST"
+        className="max-w-md mx-auto px-[3rem] py-[2rem] bg-white shadow-md rounded-[1rem] bg-[#bbdafa] text-[#0B0019] font-semibold font-primary"
+      >
+        <label className="block mb-2" htmlFor="name">
+          Name
+        </label>
+        <input
+          required
+          className="mb-4 w-full p-2 border border-gray-300 rounded-md outline-none bg-[#0B0019] text-[#EEF3FF]"
+          type="text"
+          name="name"
+          id="name"
+        />
+
+        <label className="block mb-2" htmlFor="email">
+          Email
+        </label>
+        <input
+          required
+          className="mb-4 w-full p-2 border border-gray-300 rounded-md outline-none bg-[#0B0019] text-[#EEF3FF]"
+          type="email"
+          name="email"
+          id="email"
+        />
+
+        <label className="block mb-2" htmlFor="location">
+          Location
+        </label>
+        <input
+          required
+          className="mb-4 w-full p-2 border border-gray-300 rounded-md outline-none bg-[#0B0019] text-[#EEF3FF]"
+          type="text"
+          name="location"
+          id="location"
+        />
+
+        <label className="block mb-2" htmlFor="password">
+          Password
+        </label>
+        <input
+          required
+          className="mb-4 w-full p-2 border border-gray-300 rounded-md outline-none bg-[#0B0019] text-[#EEF3FF]"
+          type="password"
+          name="password"
+          id="password"
+        />
+
+        <label className="block mb-2" htmlFor="confirmPassword">
+          Confirm Password
+        </label>
+        <input
+          required
+          className="mb-4 w-full p-2 border border-gray-300 rounded-md outline-none bg-[#0B0019] text-[#EEF3FF]"
+          type="password"
+          name="confirmPassword"
+          id="confirmPassword"
+        />
+
+        <button
+          type="submit"
+          className="w-full p-2 bg-[#f8aa26] text-[#080909] rounded-md hover:text-[#FEFFC0] hover:bg-[#0B0019] uppercase font-semibold"
+        >
+          Register
+        </button>
       </Form>
     </div>
-  )
+  );
 }
