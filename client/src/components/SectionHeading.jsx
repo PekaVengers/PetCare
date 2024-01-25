8d773adaa25aa31d7a23064f8bd2e0fee4a1fe6b
singodiyashubham87 <singodiyashubham87@gmail.com>
2024-01-25 12:30:32 +0530

    feat: pets page done

---
+++ client/src/components/SectionHeading.jsx
@@ -0,0 +1,22 @@
+import dogPaw from "../assets/images/dogPaw.png";
+
+const SectionHeading = (props) => {
+  // eslint-disable-next-line react/prop-types
+  const { heading, styles } = props;
+  return (
+    <h1
+      className={`${
+        styles ? styles : ""
+      } relative text-[6rem] font-primary uppercase font-bold my-[1rem]`}
+    >
+      {heading}{" "}
+      <img
+        src={dogPaw}
+        alt="dogPaw"
+        className="w-[5rem] absolute top-[-0.5rem] right-[-2.5rem] rotate-[30deg]"
+      />
+    </h1>
+  );
+};
+
+export default SectionHeading;
